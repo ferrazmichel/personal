7f017fd1a3c961c103a1c94d4eddcd49cf50891a
Michel <ferrazmichel@gmail.com>
2021-08-26 14:33:34 -0300

    ajuste

---
+++ nome-do-projeto/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Exercicio from './Component'
+
+function App() {
+  return <div>
+    <Exercicio />
+    <img className='App-logo' src={logo} alt='logo'></img>
+    </div>
+}
+
+export default App;
---
+++ nome-do-projeto/src/Component.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class Exercicio extends React.Component {
+  render() {
+    return <div>
+      <h1>Michel Ferraz</h1>
+      <p>Eu sou o cara!!</p>
+      </div>
+  }
+}
+
+export default Exercicio;
